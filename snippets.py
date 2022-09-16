e71f8f928cfdc164e572aa8cd964b0364853bece
adiaz-banyan <86432508+adiaz-banyan@users.noreply.github.com>
2022-09-16 15:38:31 -0700

    weird error with inputs, instructions are too vague

--- snippets.py
+++ snippets.py
@@ -1,14 +1,13 @@
-
-'''
+"""
 Debug the following code snippets so that they all run successfully
-'''
+"""
 
 
 def snippet_1():
     u = 5
     v = 2
 
-    if u * v = 10:
+    if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
@@ -23,10 +22,10 @@ def snippet_2():
     if z < x:
         print("z is less than x")
 
-    elif z > x and z < y
+    elif z > x and z < y:
         print("z is between x and y")
 
-    else
+    else:
         print("z is greater than y")
 
 
@@ -36,19 +35,20 @@ def snippet_3():
 
     a = 1
     b = 1
-    c = (a > b)
+    c = a == b
 
-    print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
-    assert(c == True)  # <-- DO NOT EDIT THIS LINE
+    print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
+    assert c == True  # <-- DO NOT EDIT THIS LINE
 
 
 def snippet_4():
     # TODO: Modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-    d = (5 < 7) or not (8 < 20)
+    d = not (5 < 7) or not (8 < 20)
 
     # TODO: Explain how d is set to False in a comment
-    assert(d == False)  # <-- DO NOT EDIT THIS LINE
+    # On line 47 both expression result in false statements. We reverse the result of both values by using the boolean operator not.
+    assert d == False  # <-- DO NOT EDIT THIS LINE
 
 
 def snippet_5():
@@ -56,9 +56,9 @@ def snippet_5():
     # TODO: Update the print statement to reflect the changes
 
     m = "GOAT"
-    n = "goat"
+    n = "GOAT"
 
-    o = (m == n)
+    o = m == n
 
-    print (f"The value of o ({o}) is True since Python is case-sensitive.")
-    assert(o == True)  # <-- DO NOT EDIT THIS LINE
+    print(f"The value of o ({o}) is True since Python is case-sensitive.")
+    assert o == True  # <-- DO NOT EDIT THIS LINE
--- stocks.py
+++ stocks.py
@@ -1,4 +1,3 @@
-
 def stock_purchases():
     amazon = 3000
     apple = 100
@@ -8,21 +7,53 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
+    # client_name = input("What is your name? ")
+    # # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
+    # # and save it into a variable
+    # invtmnt = int(input("How much would you like to invest? $"))
+    # # NOTE: When you use the `input` function to get user input, what do numbers get saved as? strings
+    # # ANSWER: strings
+    # # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
+    # # NOTE: Take a look at how this input string prints out
+    # stock = input(
+    #     "\nWhich stock are you interested in? Enter the corresponding number:\n1. Amazon\n2. Apple\n3. Facebook\n4. Google\n5. Microsoft\nNumber: "
+    # )
+    affordable = 0
+    service = ""
+    currentPrice = ""
 
-    # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
-    # and save it into a variable
-    # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
+    if stock == "1":
+        service = "Amazon"
+        currentPrice = f"${amazon}"
+        affordable = int(invtmnt / amazon)
+    elif stock == "2":
+        service = "Apple"
+        currentPrice = f"${apple}"
+        affordable = int(invtmnt / apple)
+    elif stock == "3":
+        service = "Facebook"
+        currentPrice = f"${fb}"
+        affordable = int(invtmnt / fb)
+    elif stock == "4":
+        service = "Google"
+        currentPrice = f"${google}"
+        affordable = int(invtmnt / google)
+    elif stock == "5":
+        service = "Microsoft"
+        currentPrice = f"${msft}"
+        affordable = int(invtmnt / msft)
 
-    # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
-    # NOTE: Take a look at how this input string prints out
-    # stock = input("\nWhich stock are you interested in? Enter the corresponding number:\n1. Amazon\n2. Apple\n3. Facebook\n4. Google\n5. Microsoft\nNumber: ")
+        print(service)
+        print(affordable)
+        print(currentPrice)
+    else:
+        service = ""
+        currentPrice = ""
+        affordable = 0
 
-    # Now that you have all of the client info, you can check how much they can purchase of the stock
-    # they're interested in.
+    print(
+        f"{client_name} has ${invtmnt} to invest and can buy {affordable} shares of {service} at the current price of {currentPrice}."
+    )
 
-    # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
-    # and save it in a variable
 
-    # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
-    # Use an f-string to print the result for the client, ala:
-    # Alex has $5000 in savings and can buy 50 shares of Apple at the current price of $100.
+stock_purchases()
