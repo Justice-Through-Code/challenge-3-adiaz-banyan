e679a0f27629732b24d7291fada431d598509ab6
adiaz-banyan <86432508+adiaz-banyan@users.noreply.github.com>
2022-11-06 14:47:00 -0700

    finished challenge 3 resubmit partner program stuff

--- stocks.py
+++ stocks.py
@@ -7,41 +7,50 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-    client_name = input("What is your name? ")
-    # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
-    # and save it into a variable
-    invtmnt = int(input("How much would you like to invest? $"))
-    # NOTE: When you use the `input` function to get user input, what do numbers get saved as? strings
-    # ANSWER: strings
-    # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
-    # NOTE: Take a look at how this input string prints out
-    stock = input(
-        "\nWhich stock are you interested in? Enter the corresponding number:\n1. Amazon\n2. Apple\n3. Facebook\n4. Google\n5. Microsoft\nNumber: "
-    )
+    user_input_dic = {
+        'client_name': input("What is your name? "),
+        'investment': int(input("How much would you like to invest? $")),
+        'stock': input(
+            "\nWhich stock are you interested in? Enter the corresponding number:\n1. Amazon\n2. Apple\n3. Facebook\n4. Google\n5. Microsoft\nNumber: "
+        )
+    }
+    # YOU KNOW I WAS EXTREMELY SKEPTICAL THIS WAS GOING TO WORK AS EXPECTED BUT IT DOES SO HURRAY ME!!
+
+    # client_name = input("What is your name? ")
+    # # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
+    # # and save it into a variable
+    # invtmnt = int(input("How much would you like to invest? $"))
+    # # NOTE: When you use the `input` function to get user input, what do numbers get saved as? strings
+    # # ANSWER: strings
+    # # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
+    # # NOTE: Take a look at how this input string prints out
+    # stock = input(
+    #     "\nWhich stock are you interested in? Enter the corresponding number:\n1. Amazon\n2. Apple\n3. Facebook\n4. Google\n5. Microsoft\nNumber: "
+    # )
     affordable = 0
     service = ""
     currentPrice = ""
 
-    if stock == "1":
+    if user_input_dic['stock'] == "1":
         service = "Amazon"
         currentPrice = f"${amazon}"
-        affordable = int(invtmnt / amazon)
-    elif stock == "2":
+        affordable = int(user_input_dic['investment'] / amazon)
+    elif user_input_dic['stock'] == "2":
         service = "Apple"
         currentPrice = f"${apple}"
-        affordable = int(invtmnt / apple)
-    elif stock == "3":
+        affordable = int(user_input_dic['investment'] / apple)
+    elif user_input_dic['stock'] == "3":
         service = "Facebook"
         currentPrice = f"${fb}"
-        affordable = int(invtmnt / fb)
-    elif stock == "4":
+        affordable = int(user_input_dic['investment'] / fb)
+    elif user_input_dic['stock'] == "4":
         service = "Google"
         currentPrice = f"${google}"
-        affordable = int(invtmnt / google)
-    elif stock == "5":
+        affordable = int(user_input_dic['investment'] / google)
+    elif user_input_dic['stock'] == "5":
         service = "Microsoft"
         currentPrice = f"${msft}"
-        affordable = int(invtmnt / msft)
+        affordable = int(user_input_dic['investment'] / msft)
 
         print(service)
         print(affordable)
@@ -52,5 +61,8 @@ def stock_purchases():
         affordable = 0
 
     print(
-        f"{client_name} has ${invtmnt} to invest and can buy {affordable} shares of {service} at the current price of {currentPrice}."
+        f"{user_input_dic['client_name']} has ${user_input_dic['investment']} to invest and can buy {affordable} shares of {service} at the current price of {currentPrice}."
     )
+
+
+# stock_purchases()
